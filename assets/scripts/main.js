d201f30c333f6df62176ee80fdbf7e5450a7b78f
Yuying-li-1 <yul006@ucsd.edu>
2021-02-05 21:55:12 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,6 @@
 // main.js
+var whichSound;
+var muted = 0;
 //When the slider position changes, the sound level of the <audio> element also changes
 var volNum1 = document.getElementById('volume-slider');
 volNum1.addEventListener("change", function(){
@@ -18,22 +20,21 @@ volNum1.addEventListener("change", function(){
     }
     else if(temp1 >= 1 && temp1 <= 33){
       img1.src="./assets/media/icons/volume-level-1.svg";
-      console.log(img1.src);
     }
     else{
       img1.src="./assets/media/icons/volume-level-0.svg";
+      muted = 1;
     }
-
 });
 
 //
 var volNum2 = document.getElementById('volume-number');
 volNum2.addEventListener("change", function(){
-    var temp2 = volNum2.value;
-    var num2 = document.getElementById('volume-sliders');
+    var temp2 = volNum2.valueAsNumber;
+    var num2 = document.getElementById('volume-slider');
     num2.valueAsNumber = temp2;
     var sound2 = document.getElementById('horn-sound');
-    sound2.volume = temp2;
+    sound2.volume = temp2 / 100;
 
     //change volume image
     var img2 = document.getElementById('volume-image');
@@ -45,18 +46,43 @@ volNum2.addEventListener("change", function(){
     }
     else if(temp2 >= 1 && temp2 <= 33){
       img2.src="./assets/media/icons/volume-level-1.svg";
-      console.log(img1.src);
     }
     else{
       img2.src="./assets/media/icons/volume-level-0.svg";
+      muted = 1;
     }
 
 });
 
+var sonA = document.getElementById('radio-air-horn');
+sonA.addEventListener("change", function(){
+  var sona = document.getElementById('horn-sound');
+  sona.scr = "./assets/media/audio/air-horn.mp3";
+});
+
+var sonC = document.getElementById('radio-car-horn');
+sonC.addEventListener("change", function(){
+  var sonc = document.getElementById('horn-sound');
+  sonc.scr = "./assets/media/audio/car-horn.mp3";
+});
+
+var sonP = document.getElementById('radio-party-horn');
+sonP.addEventListener("change", function(){
+  var sonp = document.getElementById('horn-sound');
+  sonp.scr = "./assets/media/audio/party-horn.mp3";
+});
+
 var playAudio = document.getElementById('honk-btn');
+playAudio.type = "button";
 playAudio.addEventListener("click", function(){
-  var miao = document.getElementById('horn-sound');
-  miao.play();
+  if(muted){
+
+  }
+
+  else{
+    var miao = document.getElementById('horn-sound');
+    miao.play();
+  }
 });
 
 //sound.volume = volNum;
