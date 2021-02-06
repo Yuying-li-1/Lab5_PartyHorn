a40ba204915b13aeab1743936dd3df2a1a68a991
Yuying-li-1 <yul006@ucsd.edu>
2021-02-05 22:14:43 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -14,16 +14,19 @@ volNum1.addEventListener("change", function(){
     var img1 = document.getElementById('volume-image');
     if(temp1 >= 67 && temp1 <= 100){
       img1.src="./assets/media/icons/volume-level-3.svg";
+          playAudio.disabled = 0;
     }
     else if(temp1 >= 34 && temp1 <= 66){
       img1.src="./assets/media/icons/volume-level-2.svg";
+          playAudio.disabled = 0;
     }
     else if(temp1 >= 1 && temp1 <= 33){
       img1.src="./assets/media/icons/volume-level-1.svg";
+          playAudio.disabled = 0;
     }
     else{
       img1.src="./assets/media/icons/volume-level-0.svg";
-      muted = 1;
+          playAudio.disabled = 1;
     }
 });
 
@@ -40,49 +43,48 @@ volNum2.addEventListener("change", function(){
     var img2 = document.getElementById('volume-image');
     if(temp2 >= 67 && temp2 <= 100){
       img2.src="./assets/media/icons/volume-level-3.svg";
+          playAudio.disabled = 0;
     }
     else if(temp2 >= 34 && temp2 <= 66){
       img2.src="./assets/media/icons/volume-level-2.svg";
+        playAudio.disabled = 0;
     }
     else if(temp2 >= 1 && temp2 <= 33){
       img2.src="./assets/media/icons/volume-level-1.svg";
+          playAudio.disabled = 0;
     }
     else{
       img2.src="./assets/media/icons/volume-level-0.svg";
-      muted = 1;
+      playAudio.disabled = 1;
     }
 
 });
 
+var sonImg = document.getElementById('sound-image');
+var miao = document.getElementById('horn-sound');
+
 var sonA = document.getElementById('radio-air-horn');
-sonA.addEventListener("change", function(){
-  var sona = document.getElementById('horn-sound');
-  sona.scr = "./assets/media/audio/air-horn.mp3";
+sonA.addEventListener("click", function(){
+  miao.src = "./assets/media/audio/air-horn.mp3";
+  sonImg.src = "./assets/media/images/air-horn.svg";
 });
 
 var sonC = document.getElementById('radio-car-horn');
-sonC.addEventListener("change", function(){
-  var sonc = document.getElementById('horn-sound');
-  sonc.scr = "./assets/media/audio/car-horn.mp3";
+sonC.addEventListener("click", function(){
+  miao.src = "./assets/media/audio/car-horn.mp3";
+  sonImg.src = "./assets/media/images/car.svg";
 });
 
 var sonP = document.getElementById('radio-party-horn');
-sonP.addEventListener("change", function(){
-  var sonp = document.getElementById('horn-sound');
-  sonp.scr = "./assets/media/audio/party-horn.mp3";
+sonP.addEventListener("click", function(){
+  miao.src = "./assets/media/audio/party-horn.mp3";
+  sonImg.src = "./assets/media/images/party-horn.svg";
 });
 
 var playAudio = document.getElementById('honk-btn');
 playAudio.type = "button";
 playAudio.addEventListener("click", function(){
-  if(muted){
-
-  }
-
-  else{
-    var miao = document.getElementById('horn-sound');
-    miao.play();
-  }
+miao.play();
 });
 
 //sound.volume = volNum;
